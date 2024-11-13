040305ebe717f8b6fd463db09a097b8992a95522
Monalisa Sabina <onyangomonalisa@hotmail.com>
2024-11-13 14:40:24 +0300

    static login page

--- project-tracker/src/pages/login.jsx
+++ project-tracker/src/pages/login.jsx
@@ -1,11 +1,40 @@
+import { Link } from "react-router-dom"
+import './login.css'
+
 function Login(){
 
     return(
         <>
             <h1>login</h1>
+
+            <div className="login-container">
+
+                <form className="login-form">
+                    <div>
+                        <label>Email adddress:</label>
+                        <input
+                            type="text"
+                            required
+                        />                   
+                    </div>
+
+                    <div>
+                        <label>Password:</label>
+                        <input
+                            type="password"
+                            required
+                        />
+                    </div>
+
+                    <button type="submit">Sign in</button>
+
+                    <p>
+                        Don't have an account? <Link to= "/signup"> Sign up here</Link>
+                    </p>
+
+                </form> 
+            </div>
         </>
     )
 }
-
-
 export default Login
